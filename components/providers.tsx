ae3f15c5f578e2e281a547d097b795702398b947
H. M. Jabed Omur Rifat <codemen.jabed@gmail.com>
2025-11-25 10:11:33 +0600

    Add NextAuth session provider component and remove React Query dependency.

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,30 +1,15 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
 
 import SessionLoader from './SessionLoader';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    const [queryClient] = useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        staleTime: 60 * 1000, // 1 minute
-                        refetchOnWindowFocus: false,
-                    },
-                },
-            })
-    );
 
     return (
         <SessionProvider>
-            <QueryClientProvider client={queryClient}>
-                <SessionLoader />
-                {children}
-            </QueryClientProvider>
+            <SessionLoader />
+            {children}
         </SessionProvider>
     );
 }
